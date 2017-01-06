3be18ed4c82f8ffc21e44198b1127744252b5fda
Brian Wilson <email@brian-wilson.org>
2017-01-06 11:54:03 +0000

    Add files via upload

---
+++ groupcsv2email.ps1
@@ -0,0 +1,40 @@
+﻿ $usercsv = import-csv 4ggacanousers.csv
+  
+ $resultsfile = "resultsacano.csv"
+ $resultsfilebanner = "Results are below...."
+
+ $resultsfilebanner > $resultsfile
+
+
+
+ foreach($user in $usercsv)
+  
+   {
+  
+   $samaccountname = $user.SamAccountName
+
+   $email = ""
+  
+    $strFilter = "(samaccountname=$samaccountname)"
+
+    $objDomain = New-Object System.DirectoryServices.DirectoryEntry
+    $objSearcher = New-Object System.DirectoryServices.DirectorySearcher
+    $objSearcher.SearchRoot = $objDomain
+    $objSearcher.PageSize = 1000
+    $objSearcher.Filter = $strFilter
+    $objSearcher.SearchScope = "Subtree"
+
+    $colProplist = "mail"
+ #   foreach ($i in $colPropList){$objSearcher.PropertiesToLoad.Add($i)}
+ $objSearcher.PropertiesToLoad.mail
+    $colResults = $objSearcher.FindAll()
+
+    foreach ($objResult in $colResults)
+   {
+   $objItem = $objResult.Properties; $objItem.mail
+   $email = $objItem.mail
+   }
+  
+   Echo "$samaccountname,$email" >> $resultsfile
+  
+   }
