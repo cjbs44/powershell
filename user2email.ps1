2c92448a82d4d6b1a23b97e685c579e286f06c68
Brian Wilson <email@brian-wilson.org>
2017-01-06 10:15:25 +0000

    Add files via upload

---
+++ user2email.ps1
@@ -0,0 +1,44 @@
+﻿ $usercsv = import-csv users.csv
+  
+ $resultsfile = "results.csv"
+ $resultsfilebanner = "Results are below...."
+
+ $resultsfilebanner > $resultsfile
+
+
+
+ foreach($user in $usercsv)
+  
+   {
+  
+   $first = $user.First
+  
+   $last = $user.Last
+
+   $last2 = $user.Last2
+
+   $email = ""
+  
+    $strFilter = "(&(objectCategory=User)(sn=$last)(givenName=$first))"
+
+    $objDomain = New-Object System.DirectoryServices.DirectoryEntry
+    $objSearcher = New-Object System.DirectoryServices.DirectorySearcher
+    $objSearcher.SearchRoot = $objDomain
+    $objSearcher.PageSize = 1000
+    $objSearcher.Filter = $strFilter
+    $objSearcher.SearchScope = "Subtree"
+
+    $colProplist = "mail"
+ #   foreach ($i in $colPropList){$objSearcher.PropertiesToLoad.Add($i)}
+ $objSearcher.PropertiesToLoad.mail
+    $colResults = $objSearcher.FindAll()
+
+    foreach ($objResult in $colResults)
+   {
+   $objItem = $objResult.Properties; $objItem.mail
+   $email = $objItem.mail
+   }
+  
+   Echo "$first, $last, $last2, $email" >> $resultsfile
+  
+   }
