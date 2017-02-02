abf807b2f7c6d4b012292c60bd70cde05b8e6625
Brian Wilson <email@brian-wilson.org>
2017-02-02 10:39:36 +0000

    Add files via upload

---
+++ DuplicatePhoneNumbers2CSV.ps1
@@ -0,0 +1,4 @@
+﻿$users = Get-ADUser -Filter { Enabled -eq $true } -Properties telephonenumber, mail
+$pnum = @{}; $users | ForEach-Object { if($_.telephonenumber) { $pnum[$_.telephonenumber] += 1 } }
+$filteredUsers = $users | Where-Object { if($_.telephonenumber) { $pnum[$_.telephonenumber] -gt 1 } }
+$filteredUsers | Select-Object -Property SamAccountName, mail, telephonenumber | Sort-Object telephonenumber | Export-Csv duplicatephonenumbers.csv
